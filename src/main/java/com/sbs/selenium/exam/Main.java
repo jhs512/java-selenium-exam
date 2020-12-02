f07dc0703e21ffbc5f373c078df71de0602bc91b
jhs512 <jangka512@gmail.com>
2020-12-02 11:34:57 +0900

    DC인사이드 식물갤 최신 게시물 정보 가져오기

--- src/main/java/com/sbs/selenium/exam/Main.java
+++ src/main/java/com/sbs/selenium/exam/Main.java
@@ -12,13 +12,80 @@ import java.util.List;
 import javax.imageio.ImageIO;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import com.sbs.selenium.exam.dto.DCInsideArticle;
+
 public class Main {
 	public static void main(String[] args) {
-		printPpomppuFreeLatestArticles();
+		printDCInsideTreeGalleryLatestArticles();
+	}
+
+	private static void printDCInsideTreeGalleryLatestArticles() {
+		Path path = Paths.get(System.getProperty("user.dir"), "src/main/resources/chromedriver.exe");
+
+		// WebDriver 경로 설정
+		System.setProperty("webdriver.chrome.driver", path.toString());
+
+		// WebDriver 옵션 설정
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--start-maximized"); // 전체화면으로 실행
+		options.addArguments("--disable-popup-blocking"); // 팝업 무시
+		options.addArguments("--disable-default-apps"); // 기본앱 사용안함
+		// options.setHeadless(true);
+
+		// WebDriver 객체 생성
+		ChromeDriver driver = new ChromeDriver(options);
+
+		// 빈 탭 생성
+		// driver.executeScript("window.open('about:blank','_blank');");
+
+		List<String> tabs = new ArrayList<String>(driver.getWindowHandles());
+
+		// 첫번째 탭으로 전환
+		driver.switchTo().window(tabs.get(0));
+		driver.get("https://gall.dcinside.com/board/lists/?id=tree");
+
+		Util.sleep(1000);
+
+		List<WebElement> elements = driver.findElements(By.cssSelector(".gall_list .us-post"));
+
+		for (WebElement element : elements) {
+			int id = Util.getAsInt(element.findElements(By.cssSelector(".gall_num")).get(0).getText().trim());
+			String title = element.findElements(By.cssSelector(".gall_tit")).get(0).getText().trim();
+			String writer = element.findElements(By.cssSelector(".gall_writer .nickname")).get(0).getText().trim();
+
+			String ipStart = "";
+
+			try {
+				WebElement ipElement = element.findElement(By.cssSelector(".gall_writer .ip"));
+				ipStart = ipElement.getText().trim();
+			} catch (NoSuchElementException e) {
+
+			}
+
+			String regDate = element.findElements(By.cssSelector(".gall_date")).get(0).getAttribute("title").trim();
+
+//			String regDate = element.findElements(By.cssSelector(".gall_date")).get(0).getText().trim();
+//			
+//			if ( regDate.contains(":") ) {
+//				regDate = Util.getNowDateStr().substring(0, 7);
+//			}
+//			else {
+//				regDate = regDate.replaceAll("\\.", ":");
+//				regDate = "20" + regDate;
+//			}
+
+			int hit = Util.getAsInt(element.findElements(By.cssSelector(".gall_count")).get(0).getText().trim());
+			int recommendsCount = Util
+					.getAsInt(element.findElements(By.cssSelector(".gall_recommend")).get(0).getText().trim());
+
+			DCInsideArticle article = new DCInsideArticle(id, title, writer, ipStart, regDate, hit, recommendsCount);
+			System.out.println(article);
+		}
 	}
 
 	private static void printPpomppuFreeLatestArticles() {
@@ -48,8 +115,8 @@ public class Main {
 
 		Util.sleep(1000);
 
-		List<WebElement> elements = driver
-				.findElements(By.cssSelector("#revolution_main_table tbody tr:not(.list_notice) > td:nth-child(3) > a"));
+		List<WebElement> elements = driver.findElements(
+				By.cssSelector("#revolution_main_table tbody tr:not(.list_notice) > td:nth-child(3) > a"));
 
 		for (WebElement element : elements) {
 			String title = element.getText().trim();
